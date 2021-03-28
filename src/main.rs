2b7442ca3bcf5e42764dcc345b8d603cdec4d137
Naoto Takezawa <naoto9march@gmail.com>
2021-03-28 15:43:16 +0900

    標準入出力とのやり取りを追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use std::io;
 mod lexer;
 
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
 fn main() {
-    let result = lexer::lex("1 + 2 * 3 - -10");
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+
+        if let Some(Ok(line)) = lines.next() {
+            if line.len() > 0 {
+                if line == "exit" || line == "quit" {
+                    prompt("bye.").unwrap();
+                    break;
+                }
+                let token = lexer::lex(&line);
+                println!("{:?}", token);
+            }
+        } else {
+            break;
+        }
+    }
 }
